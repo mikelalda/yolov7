5b50f9010493778eba5039b3015f4f11161769ad
Mikel Aldalur <maldalur@ikerlan.es>
2023-10-09 11:00:26 +0200

    Adding 2 bbox topics

--- ros_detect.py
+++ ros_detect.py
@@ -22,38 +22,47 @@ class ObjectDetection(Node):
         self.declare_parameter('weights', '"./models/yolov7.pt"')
         self.declare_parameter('topic_left', '/camera/left/image_raw')
         self.declare_parameter('topic_right', '/camera/right/image_raw')
+        self.declare_parameter('det_conf_thres', 0.8)
         self.declare_parameter('conf_thres', 0.25)
         self.declare_parameter('iou_thres', 0.45)
         self.declare_parameter('device', "0")
         self.declare_parameter('img_size', 640)
         self.declare_parameter('inf_image', False)
-        self.declare_parameter('inf_image_topic', '/object_detection/inf_image')
-        self.declare_parameter('bbox_topic', '/object_detection/bboxes')
+        self.declare_parameter('inf_left_image_topic', '/object_detection/left_inf_image')
+        self.declare_parameter('bbox_left_topic', '/object_detection/bboxes')
+        self.declare_parameter('inf_image_topic', '/object_detection/right_inf_image')
+        self.declare_parameter('bbox_right_topic', '/object_detection/bboxes')
         self.declare_parameter('frequency', 40)
 
         self.weights = self.get_parameter('weights').value
         self.topic_left = self.get_parameter('topic_left').value
         self.topic_right = self.get_parameter('topic_right').value
+        self.det_conf_thres = self.get_parameter('det_conf_thres').value
         self.conf_thres = self.get_parameter('conf_thres').value
         self.iou_thres = self.get_parameter('iou_thres').value
         self.device = self.get_parameter('device').value
         self.img_size = self.get_parameter('img_size').value
         self.inf_image = self.get_parameter('inf_image').value
-        self.inf_image_topic = self.get_parameter('inf_image_topic').value
-        self.bbox_topic = self.get_parameter('bbox_topic').value
+        self.inf_left_image_topic = self.get_parameter('inf_left_image_topic').value
+        self.bbox_left_topic = self.get_parameter('bbox_left_topic').value
+        self.inf_right_image_topic = self.get_parameter('inf_right_image_topic').value
+        self.bbox_right_topic = self.get_parameter('bbox_right_topic').value
         self.frequency = self.get_parameter('frequency').value
 
-        self.get_logger().info("weights: %s, topic_left: %s, topic_right: %s, conf_thres: %s, iou_thres: %s, device: %s, img_size: %s, inf_image: %s, inf_image_topic: %s, bbox_topic: %s, frequency: %s" %
+        self.get_logger().info("weights: %s, topic_left: %s, topic_right: %s, det_conf_thres: %s, conf_thres: %s, iou_thres: %s, device: %s, img_size: %s, inf_image: %s, inf_left_image_topic: %s, bbox_left_topic: %s, inf_right_image_topic: %s, bbox_right_topic: %s, frequency: %s" %
                     (str(self.weights),
                         str(self.topic_left),
                         str(self.topic_right),
+                        str(self.det_conf_thres),
                         str(self.conf_thres),
                         str(self.iou_thres),
                         str(self.device),
                         str(self.img_size),
                         str(self.inf_image),
-                        str(self.inf_image_topic),
-                        str(self.bbox_topic),
+                        str(self.inf_left_image_topic),
+                        str(self.bbox_left_topic),
+                        str(self.inf_right_image_topic),
+                        str(self.bbox_right_topic),
                         str(self.frequency)))
 
         # Camera info and frames
@@ -70,8 +79,10 @@ class ObjectDetection(Node):
         # Publishers for Classes
         qos_pub = QoSProfile(depth=1) 
         qos_pub.reliability = QoSReliabilityPolicy.BEST_EFFORT
-        self.infer_pub = self.create_publisher(Image, self.inf_image_topic, qos_profile=qos_pub)
-        self.bb_pub = self.create_publisher(BoundingBox2DArray, self.bbox_topic, qos_profile=qos_pub)
+        self.infer_left_pub = self.create_publisher(Image, self.inf_left_image_topic, qos_profile=qos_pub)
+        self.infer_right_pub = self.create_publisher(Image, self.inf_right_image_topic, qos_profile=qos_pub)
+        self.bb_left_pub = self.create_publisher(BoundingBox2DArray, self.bbox_left_topic, qos_profile=qos_pub)
+        self.bb_right_pub = self.create_publisher(BoundingBox2DArray, self.bbox_right_topic, qos_profile=qos_pub)
  
         # OpenCv - ROS 2 Bridge
         self.bridge = CvBridge()
@@ -107,7 +118,6 @@ class ObjectDetection(Node):
     def left_callback(self, data):
         """
         Subscription to the compressed RGB camera topic.
-
         """
         self.left_image = self.bridge.imgmsg_to_cv2(data)
         self.left_RGB = True
@@ -115,14 +125,15 @@ class ObjectDetection(Node):
     def right_callback(self, data):
         """
         Subscription to the compressed RGB camera topic.
-
         """
         self.right_image = self.bridge.imgmsg_to_cv2(data)
         self.right_RGB = True
         #self.YOLOv7_detect()
 
     def YOLOv7_detect(self):
-        """ Preform object detection with YOLOv7"""
+        """ 
+        Preform object detection with YOLOv7
+        """
         im0s = [self.left_image.copy(), self.right_image.copy]
 
         img = letterbox(self.left_image, self.img_size, self.stride)[0]
@@ -143,32 +154,30 @@ class ObjectDetection(Node):
                 self.model(img)[0]
 
         # Inference
-        t1 = time_synchronized()
+        # t1 = time_synchronized()
         with torch.no_grad():   # Calculating gradients would cause a GPU memory leak
             pred = self.model(img)[0]
-        t2 = time_synchronized()
+        # t2 = time_synchronized()
 
         # Apply NMS
         pred = non_max_suppression(pred, self.conf_thres, self.iou_thres)
-        t3 = time_synchronized()
+        # t3 = time_synchronized()
 
         # Initialize array of bounding boxes
         bb_array = BoundingBox2DArray()
 
-        # Process detections   
+        # Process detections
+        bbox_array = []
+        inf_images = []
         for i, det in enumerate(pred):  # detections per image
-            gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            im0 = im0s[i].copy()
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(img.shape[2:], det[:, :4], im0.shape).round()
 
-                # Print results
-                for c in det[:, -1].unique():
-                    n = (det[:, -1] == c).sum()  # detections per class
-
                 # Write results
                 for *xyxy, conf, cls in reversed(det):
-                    if conf > 0.8: # Limit confidence threshold to 80% for all classes
+                    if conf > self.det_conf_thres: # Limit confidence threshold to 80% for all classes
                         # Draw a boundary box around each object
                         label = f'{self.names[int(cls)]} {conf:.2f}'
                         plot_one_box(xyxy, im0, label=label, color=self.colors[int(cls)], line_thickness=2)
@@ -180,23 +189,30 @@ class ObjectDetection(Node):
                         bb.size_x = abs(bb.center.position.x/2)
                         bb.size_y = abs(bb.center.position.y/2)
                         bb_array.boxes.append(bb)
-
+            bbox_array.append(bb_array)
+            inf_images.append(im0)
             #cv2.imshow("YOLOv7 Object detection result RGB", cv2.cvtColor(cv2.resize(im0, None, fx=1.5, fy=1.5),cv2.COLOR_RGB2BGR)) 
             #cv2.waitKey()  
 
         # Publish bounding boxes
-        self.bb_pub.publish(bb_array)
+        self.bb_left_pub.publish(bbox_array[0])
+        self.bb_right_pub.publish(bbox_array[1])
         
         # Publish output ROS 2 image
-        out_img = self.bridge.cv2_to_imgmsg(cv2.cvtColor(cv2.resize(im0, None, fx=1.5, fy=1.5),cv2.COLOR_RGB2BGR))
-        self.infer_pub.publish(out_img)
+        if self.inf_image:
+            out_img = self.bridge.cv2_to_imgmsg(cv2.cvtColor(cv2.resize(inf_images[0], None, fx=1.5, fy=1.5),cv2.COLOR_RGB2BGR))
+            self.infer_left_pub.publish(out_img)
+            out_img = self.bridge.cv2_to_imgmsg(cv2.cvtColor(cv2.resize(inf_images[1], None, fx=1.5, fy=1.5),cv2.COLOR_RGB2BGR))
+            self.infer_right_pub.publish(out_img)
 
     def timer_callback(self):
         if self.camera_left == True and self.camera_right == True:
             self.YOLOv7_detect()
 
 def main(args=None):
-    """Run the main function."""
+    """
+    Run the main function.
+    """
     rclpy.init(args=args)
     with torch.no_grad():
         node = ObjectDetection()
